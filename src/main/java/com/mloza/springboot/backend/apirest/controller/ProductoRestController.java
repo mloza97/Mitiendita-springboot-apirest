e14625f60ac0ddb3ca7049128e00d38c1026e3b8
mloza97 <hector19865@hotmail.com>
2021-03-11 18:24:31 -0600

    Actualizacion de metodos

--- src/main/java/com/mloza/springboot/backend/apirest/controller/ProductoRestController.java
+++ src/main/java/com/mloza/springboot/backend/apirest/controller/ProductoRestController.java
@@ -1,9 +1,13 @@
 package com.mloza.springboot.backend.apirest.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,30 +37,103 @@ public class ProductoRestController {
 	
 	@GetMapping("/productos/{id}")
 	@ResponseStatus(HttpStatus.OK)
-	public Producto show(@PathVariable Long id) {
-		return productoService.findById(id);
+	public ResponseEntity<?> show(@PathVariable Long id) {
+		Producto producto = null;
+		Map<String, Object> response = new HashMap<>();
+		
+		try {
+			producto = productoService.findById(id);
+		} catch (DataAccessException e) {
+			response.put("mensaje", "Error al hacer la peticion con el servidor, intentelo de nuevo más tarde.");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+		if (producto == null) {
+			response.put("mensaje", "El producto ID: ".concat(id.toString()).concat(" no se encuentra registrado."));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+		}
+
+		return new ResponseEntity<Producto>(producto, HttpStatus.OK);
 	}
 	
 	@PostMapping("/productos")
 	@ResponseStatus(HttpStatus.CREATED)
-	public Producto create(@RequestBody Producto producto) {
-		return productoService.save(producto);
+	public ResponseEntity<?> create(@RequestBody Producto producto) {
+		
+		Producto productoNew = null;
+		Map<String, Object> response = new HashMap<>();
+
+		try {
+			productoNew = productoService.save(producto);
+		} catch (DataAccessException e) {
+			response.put("mensaje", "Error al hacer el insert en la base de datos, intentelo de nuevo más tarde.");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+		response.put("mensaje", "El producto ha sido creado con exito.");
+		response.put("producto", productoNew);
+
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
 	}
 	
 	@PutMapping("/productos/{id}")
-	public Producto update(@RequestBody Producto producto, @PathVariable Long id) {
-		Producto productoActual = productoService.findById(id);
+	public ResponseEntity<?> update(@RequestBody Producto producto, @PathVariable Long id) {
+		Producto productoUpdate = null;
+		Producto productoActual = null;
+
+		Map<String, Object> response = new HashMap<>();
+
+		try {
+			productoActual = productoService.findById(id);
+		} catch (DataAccessException e) {
+			response.put("mensaje", "Error al hacer la peticion con el servidor, intentelo de nuevo más tarde.");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+		if (productoActual == null) {
+			response.put("mensaje", "No es posible editar el producto ID: ".concat(id.toString())
+					.concat(" porque no se encuentra registrado."));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+		}
+		try {
+			productoActual.setNombre(producto.getNombre());
+			productoActual.setDescripcion(producto.getDescripcion());
+			productoActual.setPrecio(producto.getPrecio());
+
+			productoUpdate = productoService.save(productoUpdate);
+		} catch (DataAccessException e) {
+			response.put("mensaje", "Error al hacer el update en la base de datos, intentelo de nuevo más tarde.");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+		response.put("mensaje", "El producto ha sido actualizado con exito.");
+		response.put("cliente", productoUpdate);
+
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
 		
-		productoActual.setNombre(producto.getNombre());
-		productoActual.setDescripcion(producto.getDescripcion());
-		productoActual.setPrecio(producto.getPrecio());
-		
-		return productoService.save(productoActual);
 	}
 	
 	@DeleteMapping("/productos/{id}")
-	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void delete(@PathVariable Long id) {
-		productoService.delete(id);
+	public ResponseEntity<?> delete(@PathVariable Long id) {
+		
+		Map<String, Object> response = new HashMap<>();
+
+		try {
+			productoService.delete(id);
+		} catch (DataAccessException e) {
+			response.put("mensaje",
+					"Error al hacer la peticion de eliminar con el servidor, intentelo de nuevo más tarde.");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+		response.put("mesnaje", "El producto se ha eliminado con exito.");
+		
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
+		
 	}
 }
